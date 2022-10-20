b91753d579d3575d1c5bc45eb88c9dfe4b81ff31
esun33 <eleanor.sun@chicagobooth.edu>
2022-10-20 20:41:15 +0000

    Initial commit some initial work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  def blank_add_form
+    render({ :template => "calculation_templates/add_form.html.erb"})
+  end
+
+  def add_results
+    @n1 = params.fetch("num_1").to_f
+    @n2 = params.fetch("num_2").to_f
+
+    @result = @n1 + @n2
+
+    render({ :template => "calculation_templates/addition_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "blank_add_form"})
+
+  get("/add", { :controller => "application", :action => "blank_add_form"})
+
+  get("/wizard_add", { :controller => "application", :action => "add_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
